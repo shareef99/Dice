a026f4911e44fa201e830f9de160704e0b53aee7
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2019-06-12 15:56:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+// console.log(randomNumber1);
+if(randomNumber1 === 1){
+    document.querySelectorAll("img")[0].src = "images/dice1.png";
+}
+else if (randomNumber1 === 2)
+{
+    document.querySelectorAll("img")[0].src = "images/dice2.png";    
+}
+else if (randomNumber1 === 3)
+{
+    document.querySelectorAll("img")[0].src = "images/dice3.png";    
+}
+else if (randomNumber1 === 4)
+{
+    document.querySelectorAll("img")[0].src = "images/dice4.png";    
+}
+else if (randomNumber1 === 5)
+{
+    document.querySelectorAll("img")[0].src = "images/dice5.png";    
+}
+else if (randomNumber1 === 6)
+{
+    document.querySelectorAll("img")[0].src = "images/dice6.png";    
+}
+else
+{
+    console.log(randomNumber1);
+}
+// second random dice
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+if(randomNumber2 === 1){
+    document.querySelectorAll("img")[1].src = "images/dice1.png";
+}
+else if (randomNumber2 === 2)
+{
+    document.querySelectorAll("img")[1].src = "images/dice2.png";    
+}
+else if (randomNumber2 === 3)
+{
+    document.querySelectorAll("img")[1].src = "images/dice3.png";    
+}
+else if (randomNumber2 === 4)
+{
+    document.querySelectorAll("img")[1].src = "images/dice4.png";    
+}
+else if (randomNumber2 === 5)
+{
+    document.querySelectorAll("img")[1].src = "images/dice5.png";    
+}
+else if (randomNumber2 === 6)
+{
+    document.querySelectorAll("img")[1].src = "images/dice6.png";    
+}
+else
+{
+    console.log(randomNumber1);
+} 
+
+// condition 
+
+if (randomNumber1 === randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+else if (randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
